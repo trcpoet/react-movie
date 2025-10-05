21f87f17cf7193b6c05427f4d3ed04a86f6d8672
Tahsin Chowdhury <tchowdhury29@gmail.com>
2025-10-05 03:52:01 -0500

    First commit

---
+++ react-movie/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold underline">Tailwind works 🎉</h1>
+        </div>
+    );
+}
